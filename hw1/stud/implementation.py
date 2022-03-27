ba5eabfbae82d6208d88f584ef4ff2088eb05695
l0r3m1psum <bellanidiego@gmail.com>
2022-03-27 18:33:48 +0200

    Removed numpy dependency.

--- hw1/stud/implementation.py
+++ hw1/stud/implementation.py
@@ -1,8 +1,7 @@
-import numpy as np
+import random
 from typing import List, Tuple
 
 from model import Model
-
 
 def build_model(device: str) -> Model:
     # STUDENT: return StudentModel()
@@ -11,7 +10,7 @@ def build_model(device: str) -> Model:
 
 
 class RandomBaseline(Model):
-    options = [
+    options: List[Tuple[int, str]] = [
         (3111, "B-CORP"),
         (3752, "B-CW"),
         (3571, "B-GRP"),
@@ -27,17 +26,16 @@ class RandomBaseline(Model):
         (203394, "O")
     ]
 
-    def __init__(self):
-        self._options = [option[1] for option in self.options]
-        self._weights = np.array([option[0] for option in self.options])
-        self._weights = self._weights / self._weights.sum()
+    def __init__(self) -> None:
+        self._options: List[str] = [option[1] for option in self.options]
+        total: int = sum(option[0] for option in self.options)
+        self._weights: List[float] = [option[0]/total for option in self.options]
 
     def predict(self, tokens: List[List[str]]) -> List[List[str]]:
         return [
-            [str(np.random.choice(self._options, 1, p=self._weights)[0]) for _x in x]
+            [random.choices(self._options, self._weights, k=1)[0] for _x in x]
             for x in tokens
         ]
-
 
 class StudentModel(Model):
 
@@ -48,3 +46,9 @@ class StudentModel(Model):
         # STUDENT: implement here your predict function
         # remember to respect the same order of tokens!
         pass
+
+def main() -> int:
+    return 0
+
+if __name__ == '__main__':
+    raise SystemExit(main())
